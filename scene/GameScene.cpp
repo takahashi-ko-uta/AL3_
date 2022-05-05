1dd0a5f1e5a8afe006b391104195f36d736c1a56
LC1B_TakahashiKouta <k021g1164@g.neec.ac.jp>
2022-05-05 21:58:26 +0900

    プロジェクト ファイルを追加します。

---
+++ scene/GameScene.cpp
@@ -0,0 +1,129 @@
+﻿#include "GameScene.h"
+#include "TextureManager.h"
+#include <cassert>
+#include "PrimitiveDrawer.h"
+#include "AxisIndicator.h"
+
+GameScene::GameScene() {}
+
+GameScene::~GameScene()
+{ 
+	delete model_;
+	delete debugCamera_;
+}
+
+void GameScene::Initialize() {
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+	debugText_ = DebugText::GetInstance();
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("mario.jpg");
+	//モデル生成
+	model_ = Model::Create();
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+	//デバックカメラの生成
+	debugCamera_ = new DebugCamera(600, 600);
+	//軸方向表示の表示を有効にする
+	AxisIndicator::GetInstance()->SetVisible(true);
+	//軸方向は参照するビュープロジェクションを指定する
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
+	//ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
+	float hen[12][2];
+	float hen2[12][2];
+}
+
+void GameScene::Update() 
+{
+	//デバックカメラの更新
+	debugCamera_->Update();
+}
+
+void GameScene::Draw() {
+	
+	
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+	//モデルカメラを連動
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
+
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+	/// 
+	//3Dモデル描画
+	//model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+	//ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
+	for (int i = 0; i < 12; i++) 
+	{
+		//PrimitiveDrawer::GetInstance()->DrawLine3d(hen[i][0], hen[i][1], Vector4(0, 70, 0, 255)); 
+		PrimitiveDrawer::GetInstance()->DrawLine3d(ten[hen2[i][0]], ten[hen2[i][1]], Vector4(0, 70, 0, 255)); 
+	}
+
+	//PrimitiveDrawer::GetInstance()->DrawLine3d(box[i % 4], box[(i + 1) % 4], Vector4(0, 70, 0, 255));			//1^4 
+	//PrimitiveDrawer::GetInstance()->DrawLine3d(box[i % 4 + 4], box[(i + 1) % 4 + 4], Vector4(0, 70, 0, 255));	//5^8
+	//PrimitiveDrawer::GetInstance()->DrawLine3d(box[i % 4], box[i % 4 +4], Vector4(0, 70, 0, 255));				//縦
+
+	/*
+	PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(0, 0, 0), Vector3(5, 0, 0), Vector4(255, 0, 0, 255));
+	PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(5, 0, 0), Vector3(5, 5, 0), Vector4(255, 0, 0, 255));
+	PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(5, 5, 0), Vector3(0, 5, 0), Vector4(255, 0, 0, 255));
+	PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(0, 5, 0), Vector3(0, 0, 0), Vector4(255, 0, 0, 255));
+
+	PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(0, 0, 5), Vector3(0, 5, 5), Vector4(255, 0, 0, 255));
+	PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(0, 0, 5), Vector3(5, 0, 5), Vector4(255, 0, 0, 255));
+	PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(5, 0, 5), Vector3(5, 5, 5), Vector4(255, 0, 0, 255));
+	PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(5, 5, 5), Vector3(0, 5, 5), Vector4(255, 0, 0, 255));
+
+	PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(0, 0, 0), Vector3(0, 0, 5), Vector4(255, 0, 0, 255));
+	PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(5, 0, 0), Vector3(5, 0, 5), Vector4(255, 0, 0, 255));
+	PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(5, 5, 0), Vector3(5, 5, 5), Vector4(255, 0, 0, 255));
+	PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(0, 5, 0), Vector3(0, 5, 5), Vector4(255, 0, 0, 255));*/
+
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// デバッグテキストの描画
+	debugText_->DrawAll(commandList);
+	//
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+	
+}
---
+++ scene/GameScene.h
@@ -0,0 +1,92 @@
+﻿#pragma once
+
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "DebugText.h"
+#include "Input.h"
+#include "Model.h"
+#include "SafeDelete.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+#include "DebugCamera.h"
+
+/// <summary>
+/// ゲームシーン
+/// </summary>
+class GameScene {
+
+  public: // メンバ関数
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	GameScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameScene();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	//3Dモデル
+	Model* model_ = nullptr;
+	//ワールドトランスフォームビュープロジェクション
+	WorldTransform worldTransform_;
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
+	//デバックカメラ
+	DebugCamera* debugCamera_ = nullptr;
+	Vector3 ten[8] = {
+	  {0.0f, 0.0f, 0.0f},
+      {5.0f, 0.0f, 0.0f},
+      {5.0f, 0.0f, 5.0f},
+      {0.0f, 0.0f, 5.0f},
+      {0.0f, 5.0f, 0.0f},
+      {5.0f, 5.0f, 0.0f},
+      {5.0f, 5.0f, 5.0f},
+      {0.0f, 5.0f, 5.0f}
+    };
+
+
+
+	int hen2[12][2] = {
+	  {0, 1},
+      {1, 2},
+      {2, 3},
+      {3, 0},
+
+	  {4, 5},
+      {5, 6},
+      {6, 7},
+      {7, 4},
+
+	  {0, 4},
+      {1, 5},
+      {2, 6},
+      {3, 7},
+	};
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+  private: // メンバ変数
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+	DebugText* debugText_ = nullptr;
+
+	/// <summary>
+	/// ゲームシーン用
+	/// </summary>
+};
