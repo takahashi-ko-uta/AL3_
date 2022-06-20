50b19c2966e14df852eb58d508695f99934113f8
LC1B_TakahashiKouta <k021g1164@g.neec.ac.jp>
2022-06-20 11:56:27 +0900

    ファイル整理

--- player/Player.cpp
+++ player/Player.cpp
@@ -1,48 +1,48 @@
 #include "Player.h"
-#include "TextureManager.h"
-#include <cassert>
-#include "PrimitiveDrawer.h"
 #include "AxisIndicator.h"
 #include "MathUtility.h"
-#include <random>
+#include "PrimitiveDrawer.h"
+#include "TextureManager.h"
 #include "affinTransformation.h"
+#include <cassert>
+#include <random>
 
-
-void Player::Initalize(Model* model, uint32_t textureHandle) 
-{
-	//NULLポインタチェック
+void Player::Initalize(Model* model, uint32_t textureHandle) {
+	// NULLポインタチェック
 	assert(model);
 
 	//引数として受け取ってデータをメンバ変数に記録する
 	model_ = model;
 	textureHandle_ = textureHandle;
 	//シングルトンインスタンスを取得する
-	input_= Input::GetInstance();
+	input_ = Input::GetInstance();
 	debugText_ = DebugText::GetInstance();
 
 	//ワールド変換の初期化
 	worldTransforms_.Initialize();
 }
 
-void Player::Update() 
-{
-#pragma region キャラクターの移動ベクトル
-	Vector3 move = {0,0,0};
-	//移動ベクトルの変更する処理
-	if (input_->PushKey(DIK_UP))
-	{
-		move.y += 0.5f; 
-	} 
-	else if(input_->PushKey(DIK_DOWN)) 
-	{
-		move.y -= 0.5f;
-	} 
-	else if (input_->PushKey(DIK_LEFT)) 
-	{
-		move.x -= 0.5f;
+void Player::Update() {
+	Move();   //移動処理
+	Rotate(); //旋回処理
+	Attack(); //攻撃処理
+	//弾更新
+	if (bullet_) {
+		bullet_->Update(worldTransforms_);
 	}
-	else if (input_->PushKey(DIK_RIGHT))
-	{
+}
+
+void Player::Move() {
+#pragma region キャラクターの移動ベクトル
+	Vector3 move = {0, 0, 0};
+	//移動ベクトルの変更する処理
+	if (input_->PushKey(DIK_UP)) {
+		move.y += 0.5f;
+	} else if (input_->PushKey(DIK_DOWN)) {
+		move.y -= 0.5f;
+	} else if (input_->PushKey(DIK_LEFT)) {
+		move.x -= 0.5f;
+	} else if (input_->PushKey(DIK_RIGHT)) {
 		move.x += 0.5f;
 	}
 
@@ -66,18 +66,50 @@ void Player::Update()
 	//行列更新
 	worldTransforms_.TransferMatrix();
 
+	//デバック
 	debugText_->SetPos(50, 50);
 	debugText_->Printf(
 	  " worldTransforms_.translation_:(%f,%f,%f)", worldTransforms_.translation_.x,
 	  worldTransforms_.translation_.y, worldTransforms_.translation_.z);
 #pragma endregion
-
-
 }
 
-void Player::Draw(ViewProjection& viewProjection) 
-{ 
+void Player::Rotate() {
+	Vector3 RotY = {0.0f, 0.0f, 0.0f};
+	if (input_->PushKey(DIK_U)) {
+		RotY.y += 0.01f;
+	} else if (input_->PushKey(DIK_I)) {
+		RotY.y -= 0.01f;
+	}
+
+	worldTransforms_.rotation_ += RotY;
+	affinTransformation::Transfer(worldTransforms_);
+	//行列更新
+	worldTransforms_.TransferMatrix();
+	//デバック
+	debugText_->SetPos(50, 70);
+	debugText_->Printf(
+	  " worldTransforms_.rotation_:(%f,%f,%f)", worldTransforms_.rotation_.x,
+	  worldTransforms_.rotation_.y, worldTransforms_.rotation_.z);
+}
+
+void Player::Attack() {
+	if (input_->PushKey(DIK_SPACE)) {
+		//弾を生成し、初期化
+		PlayerBullet* newBullet = new PlayerBullet();
+		newBullet->Initialize(model_, worldTransforms_.translation_);
+
+		//弾を登録する
+		bullet_ = newBullet;
+	}
+}
+
+void Player::Draw(ViewProjection& viewProjection) {
 	model_->Draw(worldTransforms_, viewProjection, textureHandle_);
+	//弾描画
+	if (bullet_) {
+		bullet_->Draw(viewProjection);
+	}
 }
 
 
